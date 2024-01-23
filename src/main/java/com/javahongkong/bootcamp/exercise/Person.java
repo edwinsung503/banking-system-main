3f32052a9544b891f980f9b1f1079fe534bc2851
edwin sung <edwardwong503@gmail.com>
2024-01-23 17:59:45 +0800

    update

---
+++ src/main/java/com/javahongkong/bootcamp/exercise/Person.java
@@ -0,0 +1,26 @@
+package com.javahongkong.bootcamp.exercise;
+
+public class Person extends AccountHolder{
+	private String firstName;
+	private String lastName;
+	// private int idNumber;
+
+	public Person(String firstName, String lastName, int idNumber) {
+		// complete the function
+		super(idNumber);
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		// complete the function
+		//return null;
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		// complete the function
+		//return null;
+		return this.lastName;
+	}
+}
